3020781f734cdb4dd528b8b48a1033bd81c825df
s33223 <s33223@pjwstk.edu.pl>
2025-04-04 16:49:58 +0200

    Initial commit

---
+++ Tutorial5/src/Playlist.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+
+public class Playlist {
+    private String name;
+    private ArrayList<String> tracks;
+
+    public Playlist(String name) {
+        this.name = name;
+        tracks = new ArrayList<>();
+
+    }
+    public String getName() {
+        return this.name;
+    }
+
+    public void addTrack(String track) {
+        if (!tracks.contains(track)) {
+            this.tracks.add(track);
+        } else {
+            System.out.println("Track " + this.name + " already exists");
+
+        }
+    }
+
+    public void removeTrack(String track) {
+        boolean isRemoved = this.tracks.remove(track);
+        if (!isRemoved) {
+            System.out.println("Track " + this.name + " does not exist");
+        }
+
+    }
+
+    public void display(){
+        System.out.println("Playlist " + this.name + " has " + this.tracks.size() + " tracks");
+        if (this.tracks.isEmpty()) {
+            System.out.println("Playlist " + this.name + " has no tracks");
+
+        }
+        else {
+            for (String track : this.tracks) {
+                System.out.println("  " + track);
+            }
+        }
+    }
+
+
+
+}
